2dd8f93fa388a8e3f33544687d3d171c069455d4
Sam Wlody <sam.wlody@rochester.edu>
2018-02-13 01:43:11 -0500

    cleanup

--- runas.c
+++ runas.c
@@ -1,16 +1,34 @@
 #define _GNU_SOURCE // setresuid, setresgid
 
-// cannot use pwd.h if you plan on using static linking
+// cannot use pwd.h if you plan on compiling with static linking
 // #include <pwd.h> // getpwuid, getpwnam
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdarg.h> // vfprintf
 #include <unistd.h> // getpass, getuid & setuid, fork
 #include <sys/wait.h> // waitpid
 
-// comment this out to only set effective uid of program
+// comment this out to only set effective uid of target program
 #define SET_REAL_UID
+
+// Just fprintf to stderr and then exit with error code
+void
+print_error_and_exit(const char *format, ...)
+{
+	// First we'll remove the target user saved uid and gid
+	uid_t my_uid = getuid();
+	gid_t my_gid = getgid();
+	setresuid(my_uid, my_uid, my_uid);
+	setresgid(my_gid, my_gid, my_gid);
+
+	va_list args;
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+	exit(1);
+}
 
 // re-definitions of pwd.h struct and functions
 // don't use these if we've included pwd.h
@@ -18,6 +36,7 @@
 #ifndef _PWD_H
 
 struct passwd {
+	// 33 bytes allows 32-byte username and null-terminator
 	char  pw_name[33];
 	uid_t pw_uid;
 	gid_t pw_gid;
@@ -38,78 +57,72 @@ struct passwd *
 getpw(const char *username, const uid_t uid)
 {
 	FILE *fp = fopen("/etc/passwd", "r");
-	if (fp == NULL) {
-		fprintf(stderr, "Could not open /etc/passwd\n");
-		exit(1);
-	}
-
+	if (fp == NULL) // This should probably never happen
+		print_error_and_exit("Could not open /etc/passwd\n");
+	struct passwd *result = NULL;
 	char buf[1024];
+
 	// Read /etc/passwd line-by-line
 	for (int line = 1; fgets(buf, 1024, fp) != NULL; line++) {
+		// First, read in username
 		char *start, *end;
 		start = buf;
 		end = strchr(buf, ':');
-		if (end == NULL) {
-			fprintf(stderr, "/etc/passwd parse error at line %d\n", line);
-			exit(1);
-		}
+		if (end == NULL)
+			print_error_and_exit("/etc/passwd parse error at line %d\n", line);
 		*end = '\0';
-
-		// First part of entry is username, which we store for now
 		char *entry_uname = start;
 
 		// If we're retrieving by username (uid == -1)
-		// we should continue the loop if the username doesn't match
+		// we should continue the loop only if the username doesn't match
 		// otherwise we'll keep going until we can check uid
-		if (uid != -1 || !strcmp(username, entry_uname)){
-			// Next is uid
-			// We ignore the password part of the entry
-			// which should always be just "x"
-			start = end + 3;
-			end = strchr(start, ':');
+		if (uid == -1 && strcmp(username, entry_uname) != 0)
+			continue;
 
-			if (end == NULL) {
-				fprintf(stderr, "/etc/passwd parse error at line %d\n", line);
-				exit(1);
-			}
+		// Now read in the uid, ignoring the password entry
+		// which should always be just "x", by skipping ahead 3 bytes
+		start = end + 3;
+		end = strchr(start, ':');
+		if (end == NULL)
+			print_error_and_exit("/etc/passwd parse error at line %d\n", line);
+		*end = '\0';
 
-			*end = '\0';
-			char *uid_str = start;
-			char *tol_end = uid_str;
-			// We convert the string to a uid_t
-			uid_t entry_uid = strtol(uid_str, &tol_end, 10);
+		// We convert the string to a uid_t
+		char *uid_str = start;
+		char *tol_end = uid_str;
+		uid_t entry_uid = strtol(uid_str, &tol_end, 10);
 
-			// If it's the uid we want, or we already have the username we want
-			// we get the gid and return a struct
-			// otherwise, we keep searching
-			if (uid == entry_uid || uid == -1) {
-				// get the gid
-				start = end + 1;
-				if (start == NULL) {
-					fprintf(stderr, "/etc/passwd parse error at line %d\n", line);
-					exit(1);
-				}
+		// If it's the uid we want, or we already have the username we want
+		// we can get the gid and return a struct
+		// otherwise, we keep searching
+		if (uid != entry_uid && uid != -1)
+			continue;
 
-				char *gid_str = start;
+		// get the gid
+		start = end + 1;
+		end = strchr(start, ':');
+		if (start == NULL)
+			print_error_and_exit("/etc/passwd parse error at line %d\n", line);
+		*end = '\0';
+		char *gid_str = start;
 
-				tol_end = gid_str;
-				// convert to a gid_t
-				gid_t entry_gid = strtol(gid_str, &end, 10);
-	
-				// create the passwd struct and return it
-				struct passwd *result = (struct passwd *)malloc(sizeof(struct passwd));
-				strcpy(result->pw_name, entry_uname);
-				result->pw_uid = entry_uid;
-				result->pw_gid = entry_gid;
+		// convert to a gid_t
+		tol_end = gid_str;
+		gid_t entry_gid = strtol(gid_str, &end, 10);
 
-				fclose(fp);
-				return result;
-			}
-		}
+		// create the passwd struct and return it
+		// this will need to be freed later
+		result = (struct passwd *)malloc(sizeof(struct passwd));
+		strcpy(result->pw_name, entry_uname);
+		result->pw_uid = entry_uid;
+		result->pw_gid = entry_gid;
+
+		break;
 	}
 
 	fclose(fp);
-	return NULL;
+	// Will return NULL if we didn't find what we were looking for
+	return result;
 }
 
 // Helper functions for getpw
@@ -129,70 +142,65 @@ getpwnam(const char *username)
 #endif
 
 /* 
+ * checks whether the current user is authorized
+ * to run programs as the target user given the
+ * password as specified in /etc/runas
+ *
  * returns:
  * 0 if user authorized
  * 1 if not authorized
- * 2 if incorrect password 
+ * 2 if incorrect password
  */
 int
-check_user_authorization(const char *current_uname,
-						 const char *target_uname, 
-						 const char *password,
-						 FILE *runas)
+check_user_authorization(const char *current_uname, const char *target_uname, const char *password, FILE *runas)
 {
 	// buffer should be more than enough
 	// for two 32-byte usernames (the maximum)
 	// plus a 255-byte password
 	char buf[384];
+
 	// Read in lines one at a time
 	for (int line = 1; fgets(buf, 384, runas) != NULL; line++) {
-		// replace newline with null-terminator
-		int length = strlen(buf);
-		if (length > 0) {
-			buf[length-1] = '\0';
-		} else {
-			fprintf(stderr, "/etc/runas parse error at line %d\n", line);
-			exit(1);
-		}
-
+		// read in username
 		char *start, *end;
 		start = buf;
 		end = strchr(start, ':');
-		if (end == NULL) {
-			fprintf(stderr, "/etc/runas parse error at line %d\n", line);
-			exit(1);
-		}
-
-		// compare username to one in current entry
+		if (end == NULL)
+			print_error_and_exit("/etc/runas parse error at line %d\n", line);
 		*end = '\0';
 		char *entry_uname = start;
-		if (strcmp(entry_uname, current_uname) == 0) {
-			// username matches
-			// compare target username to one in current entry
-			start = end + 1;
-			end = strchr(start, ':');
-			if (end == NULL) {
-				fprintf(stderr, "/etc/runas parse error at line %d\n", line);
-				exit(1);
-			}
-			*end = '\0';
-			char *entry_target = start;
-			if (strcmp(entry_target, target_uname) == 0) {
-				// target username matches
-				// compare password to one in current entry
-				start = end + 1;
-				if (start == NULL) {
-					fprintf(stderr, "/etc/runas parse error at line %d\n", line);
-					exit(1);
-				}
-				char *entry_pw = start;
-				// return 0 if the password matches, otherwise return 2
-				if (strcmp(entry_pw, password) == 0)
-					return 0;
-				else
-					return 2;
-			}
-		}
+
+		// compare username to one in current entry
+		// if it's not our current username, keep searching
+		if (strcmp(entry_uname, current_uname) != 0)
+			continue;
+
+		// username matches
+		// get the target username
+		start = end + 1;
+		end = strchr(start, ':');
+		if (end == NULL)
+			print_error_and_exit("/etc/runas parse error at line %d\n", line);
+		*end = '\0';
+		char *entry_target = start;
+
+		// compare target username to one in current entry
+		// if it's not the one we're looking for, keep searching
+		if (strcmp(entry_target, target_uname) != 0)
+			continue;
+
+		// target username matches
+		// get the password
+		start = end + 1;
+		end = strchr(start, '\n');
+		if (end == NULL)
+			print_error_and_exit("/etc/runas parse error at line %d\n", line);
+		*end = '\0';
+		char *entry_pw = start;
+
+		// compare password to one in current entry
+		// return 0 if the password matches, otherwise return 2
+		return strcmp(entry_pw, password) == 0 ? 0 : 2;
 	}
 
 	return 1;
@@ -208,10 +216,8 @@ main(int argc, char *argv[])
 		printf("Usage:\n");
 		printf("%s <username> <program> [arg1 arg2 arg3 ...]\n\n", argv[0]);
 		printf("Options:\n");
-		printf("username: the username that you want to %s",
-			   "run the specified program as\n");
-		printf("program: the program that you want to run, %s",
-			   "followed by the arguments to the program\n");
+		printf("username: the username that you want to run the specified program as\n");
+		printf("program: the program that you want to run, followed by the arguments to the program\n");
 		printf("-h: show this text\n");
 		exit(1);
 	}
@@ -220,46 +226,34 @@ main(int argc, char *argv[])
 	// This works because of the way Linux handles file streams
 	// Once a stream is open, it doesn't check the permissions!
 	// We still want to close the stream as quickly as possible though
-	FILE *fp = fopen("/etc/runas", "r");
-	if (fp == NULL) {
-		fprintf(stderr, "Could not open /etc/runas\n");
-		exit(1);
-	}
+	FILE *runas = fopen("/etc/runas", "r");
+	if (runas == NULL)
+		print_error_and_exit("Could not open /etc/runas\n");
 
 	// Do the same for /var/tmp/runaslog
 	// If we can't open it, we don't worry about it until the end
-	FILE *log = fopen("/var/tmp/runaslog", "a");		
+	FILE *log = fopen("/var/tmp/runaslog", "a");        
 
 	// Here we get the target user info
 	const struct passwd *target_user = getpwnam(argv[1]);
-	if (target_user == NULL) {
-		// this could happen realistically
-		setresuid(-1, getuid(), getuid());
-		fprintf(stderr, "Target user %s not found in /etc/passwd\n", argv[1]);
-		exit(1);
-	}
+	if (target_user == NULL) // this could happen realistically
+		print_error_and_exit("Target user %s not found in /etc/passwd\n", argv[1]);
 
 	/******************
 	* Drop priveleges *
 	******************/
 	// Now we can already completely drop root priveleges
 	// but first we place the target uid into the saved uid
-	// now use their priveleges later, if the current user is authorized
+	// so we can use their priveleges later
 	setresuid(-1, getuid(), target_user->pw_uid);
 	setresgid(-1, getgid(), target_user->pw_gid);
 
 	// We put as little code as possible before this privelege drop
 
-
 	// Here we get the current (real) user info
 	const struct passwd *current_user = getpwuid(getuid());
-	if (current_user == NULL) {
-		// this should probably never happen
-		setresuid(-1, -1, getuid());
-		fprintf(stderr, "Current user with uid %lu not found in /etc/passwd\n",
-				(unsigned long int) current_user->pw_uid);
-		exit(-1);
-	}
+	if (current_user == NULL) // this should probably never happen
+		print_error_and_exit("Current user with uid %lu not found in /etc/passwd\n", (unsigned long int) current_user->pw_uid);
 
 	// Retrieve password from stdin
 	const char *password = getpass("Password: ");
@@ -268,24 +262,17 @@ main(int argc, char *argv[])
 	* Check user credentials *
 	*************************/
 	// This is as early as we can possibly check credentials
-	int auth = check_user_authorization(current_user->pw_name,
-										target_user->pw_name,
-										password, fp);
+	int auth = check_user_authorization(current_user->pw_name, target_user->pw_name, password, runas);
 	// Now we can close the /etc/runas stream
-	// but /var/tmp/runaslog is still open
-	fclose(fp);
+	// but /var/tmp/runaslog is still open!
+	fclose(runas);
 
 	// if not authorized, exit with error code
 	// otherwise we will continue to execution
-	if (auth == 2) {
-		fprintf(stderr, "Incorrect password entered\n");
-		exit(1);
-	}
-	if (auth == 1) {
-		fprintf(stderr, "User %s not authorized to run programs as %s\n",
-				current_user->pw_name, target_user->pw_name);
-		exit(1);
-	}
+	if (auth == 2)
+		print_error_and_exit("Incorrect password entered\n");
+	if (auth == 1)
+		print_error_and_exit("User %s not authorized to run programs as %s\n", current_user->pw_name, target_user->pw_name);
 	
 	/********************
 	*  Execute program  *
@@ -301,10 +288,8 @@ main(int argc, char *argv[])
 	pid_t pid;
 	errno = 0;
 	// Fork off new process
-	if ((pid = fork()) < 0) {
-		fprintf(stderr, "Unable to fork new process\n%s\n", strerror(errno));
-		exit(-1);
-	}
+	if ((pid = fork()) < 0)
+		print_error_and_exit("Unable to fork new process\n%s\n", strerror(errno));
 	if (pid == 0) {
 		// Child process
 		// Now we set the effective uid & gid to that of target user
@@ -313,6 +298,7 @@ main(int argc, char *argv[])
 		setegid(target_user->pw_gid);
 
 		// Can only set real uid when unpriveleged by swapping with effective uid
+		// Then we can just set the effective uid back to what it was before swapping
 		// we shouldn't need to set real uid unless the executed program has
 		// a call to access(2), which check real uid instead of effective uid
 		#ifdef SET_REAL_UID
@@ -325,35 +311,30 @@ main(int argc, char *argv[])
 		// Print some info:
 
 		// struct passwd *real_user = getpwuid(getuid());
-		// struct passwd *eff_user = getpwuid(geteuid());		
+		// struct passwd *eff_user = getpwuid(geteuid());       
 		// printf("Real user / uid:\t %s / %lu\nEffective user / uid:\t %s / %lu\n",
-		// 		real_user->pw_name, (unsigned long int) real_user->pw_uid,
-		// 		eff_user->pw_name, (unsigned long int) eff_user->pw_uid);
+		//      real_user->pw_name, (unsigned long int) real_user->pw_uid,
+		//      eff_user->pw_name, (unsigned long int) eff_user->pw_uid);
 
 		// execute program with arguments
 		execvp(exec_args[0], exec_args);
 
 		// If we get to here, the execution failed
-		fprintf(stderr, "Unable to execute program %s\n%s\n",
-				exec_args[0], strerror(errno));
 		// Kill the process so it doesn't execute further
-		setresuid(-1, -1, getuid());
-		exit(-1);
+		print_error_and_exit("Unable to execute program %s\n%s\n", exec_args[0], strerror(errno));
 	}
 
 	// Now we can reset saved uid to our real uid
 	setresuid(-1, -1, getuid());
 
 	// Wait for child process or exit with error
-	if ((pid = waitpid(pid, &status, 0)) < 0) {
-		fprintf(stderr, "waitpid failure\n%s\n", strerror(errno));
-		exit(-1);
-	}
+	if ((pid = waitpid(pid, &status, 0)) < 0)
+		print_error_and_exit("waitpid failure\n%s\n", strerror(errno));
 
 	/******************
 	*  Log execution  *
-	******************/	
-	// append exit status
+	******************/ 
+	// append exit status to /var/tmp/runaslog if it is writable
 	if (log != NULL) {
 		fprintf(log, "%d", WIFEXITED(status) ? 0 : WEXITSTATUS(status));
 		// append program name and arguments
@@ -363,10 +344,11 @@ main(int argc, char *argv[])
 		fclose(log);
 	} else {
 		fprintf(stderr, "Could not open /var/tmp/runaslog\n");
-		int success = WIFEXITED(status);
-		printf("%s exited %s with return code %d\n", exec_args[0],
-				success ? "successfully" : "unsuccessfully",
-				success ? 0 : WEXITSTATUS(status));
+		// Since we can't log to file, we'll output exit status to terminal
+		if (WIFEXITED(status))
+			printf("%s exited successfully\n", exec_args[0]);
+		else
+			printf("%s exited unsuccessfully with return code %d\n", exec_args[0], WEXITSTATUS(status));
 	}
 
 	exit(0);
